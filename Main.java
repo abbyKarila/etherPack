fb4c6b76537f41b694670af2d55822f827273263
Abby K Peterson <abbyKarila@users.noreply.github.com>
2017-04-19 13:26:13 -0500

    added raw socket starter code

--- Main.java
+++ Main.java
@@ -2,58 +2,79 @@ package etherPack0;
 
 import java.io.IOException;
 import java.net.*;
+import com.savarese.rocksaw.net.RawSocket;
+import static com.savarese.rocksaw.net.RawSocket.PF_INET;
+import static com.savarese.rocksaw.net.RawSocket.getProtocolByName;
 
 public class Pack {
 
-	public static void main(String newDestinationIP, String newSourceIP, String newPayload) throws UnknownHostException, SocketException, IOException  {
+  public static class Packer {
+    protected RawSocket socket;
+	protected InetAddress address;
+	protected byte[] data;
+	protected int offset, len;
+	
+    protected DatagramPacket packet;
+	protected byte[] payload;
+	protected byte[] sourceIP;
+	protected byte[] destinationIP;
+	protected int destinationPort;
+
+    protected Packer() {
+	  try { address = InetAddress.getByAddress(destinationIP); }
+	  catch (UnknownHostException uhe) {}
+	  
+      packet = new DatagramPacket(payload, payload.length, address, destinationPort);
+      //packet.setIPHeaderLength(5);
+      //packet.setUDPDataByteLength(56);
+	  
+	  //data		 = packet.getUDPPacketData();
+      //offset     = packet.getIPHeaderByteLength();
+      //len        = packet.getUDPPacketByteLength();
+    }
+	
+	protected void send() {
+	  socket = new RawSocket();
+	  try { 
+		socket.open(PF_INET, getProtocolByName("udp"));
+		socket.write(address, data, offset, len);
+		socket.close();
+	  } catch (IOException ioe) {}
+	  
+	}
+
+	public static final void main(String dIP, String sIP, String pay) throws UnknownHostException, SocketException, IOException  {
 		
-		/* packet fields */
-		byte[] payload = 		{ 66, 66, 69, 78, 66, 69, 78, 66, 69, 78 };
-		byte[] sourceIP = 		{ (byte)198, (byte)168, (byte)1, (byte)15 };
-		byte[] destinationIP =	{ (byte)198, (byte)168, (byte)1, (byte)15 };
+		/* create packet */
+		final Packer p = new Pack.Packer();
 		
-		int destinationPort = 57;
+		/* assemble packet */
 		String seperator = "[.]+";
-		
-		/* gather inputs */
-		/*Scanner input = new Scanner(System.in);
-		System.out.println("Destination IP:\n");
-		String newDestinationIP = input.next();
-		System.out.println("Payload:\n");
-		String newPayload = input.next();
-		System.out.println("Source IP:\n");
-		String newSourceIP = input.next();
-		input.close();*/
-		
-		/* parse into byte arrays */
-		String[] dipTokens = newDestinationIP.split(seperator);
+		String[] dipTokens = dIP.split(seperator);
 		for (int i=0; i<dipTokens.length; i++) {
 			int test = Integer.parseInt(dipTokens[i]);
-			destinationIP[i] = (byte) (test & 0xFF);	// masking the int with FF allows us to use "unsigned" byte up to 255
+			p.destinationIP[i] = (byte) (test & 0xFF);	// masking the int with FF allows us to use "unsigned" byte up to 255
 		}
 		
-		char[] payloadTokens = newPayload.toCharArray();
+		char[] payloadTokens = pay.toCharArray();
 		for (int i=0; i<payloadTokens.length; i++) {
 			int test = (int) payloadTokens[i];
-			payload[i] = (byte) (test & 0xFF);
+			p.payload[i] = (byte) (test & 0xFF);
 		}
 		
-		/* TODO: look into how to edit sender IP */
-		String[] sipTokens = newSourceIP.split(seperator);
+		String[] sipTokens = sIP.split(seperator);
 		for (int i=0; i<sipTokens.length; i++) {
 			int test = Integer.parseInt(sipTokens[i]);
-			sourceIP[i] = (byte) (test & 0xFF);
+			p.sourceIP[i] = (byte) (test & 0xFF);
 		}
-			
-		/* send the UDP packet using java.net utilities */
-        InetAddress address = InetAddress.getByAddress(destinationIP);
-        DatagramPacket packet = new DatagramPacket(payload, payload.length, address, destinationPort);
-        DatagramSocket socket = new DatagramSocket();
-        socket.send(packet);
-
-        //System.out.println("Sent");
-              
-        socket.close();
+		
+		p.destinationPort = 57;
+		
+		/* send packet */
+		p.send();
+        System.out.println("Sent");
 	}
-	
+  }
+  
 }
+
